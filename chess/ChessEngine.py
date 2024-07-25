d8fad0cfa30f23fc191b3e6336bc02ded3a64a56
Vidhu-sri <118075691+Vidhu-sri@users.noreply.github.com>
2024-07-26 00:40:56 +0530

    defined driver code

---
+++ chess/ChessEngine.py
@@ -0,0 +1,26 @@
+
+#to handle current state of the board
+#determines valid moves as well
+
+
+
+#TO TRACK
+
+#whose turn is it
+#movelog
+#can castle?
+
+class GameState:
+    def __init__(self):
+        
+        self.board =  [['bR', 'bN', 'bB', 'bQ', 'bK', 'bB', 'bN', 'bR'],
+                ['bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp'],
+                ['--', '--', '--', '--', '--', '--', '--', '--'],
+                ['--', '--', '--', '--', '--', '--', '--', '--'],
+                ['--', '--', '--', '--', '--', '--', '--', '--'],
+                ['--', '--', '--', '--', '--', '--', '--', '--'],
+                ['wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp'],
+                ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR']]
+        self.whiteToMove = True
+        self.movelog = []
+        
--- chess/ChessMain.py
+++ chess/ChessMain.py
@@ -1,19 +1,67 @@
+import pygame as p
+import ChessEngine
 
-#to handle current state of the board
-#determines valid moves as well
+
+#TO DEFINE
+# - size
+# - dimension
+# - sq.size
+# - max fps
+# - global dictionary of images
+p.init()
+WIDTH = HEIGHT = 512
+DIMENSION = 8
+SQ_SIZE = WIDTH//DIMENSION
+MAX_FPS = 20
+IMAGES = {}
 
 
-class board:
-    def __init__(self):
-        
-        self.baord =  [['bR', 'bN', 'bB', 'bQ', 'bK', 'bB', 'bN', 'bR'],
-                ['bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp'],
-                ['--', '--', '--', '--', '--', '--', '--', '--'],
-                ['--', '--', '--', '--', '--', '--', '--', '--'],
-                ['--', '--', '--', '--', '--', '--', '--', '--'],
-                ['--', '--', '--', '--', '--', '--', '--', '--'],
-                ['wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp'],
-                ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR']]
-        self.whiteToMove = True
-        self.movelog = []
-        
+def loadImages():
+    pieces = ['bR', 'bN', 'bB', 'bQ', 'bK', 'bB', 'bN', 'bR','bp', 'wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR','wp']
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load(f'images/{piece}.png'), (SQ_SIZE,SQ_SIZE))
+
+#driver code
+def main():
+
+    screen = p.display.set_mode((WIDTH,HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color('white'))
+    gs = ChessEngine.GameState()
+    loadImages()
+    running = True
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+
+        drawGameState(screen,gs)
+        drawPieces(screen,gs.board)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+def drawGameState(screen,gs):
+    drawBoard(screen)
+    drawPieces(screen,gs.board)
+
+
+#drawboard , drawpieces can be handled in a single function 
+def drawBoard(screen):
+    colors = [p.Color('white'), p.Color('gray')]
+
+    for i in range(DIMENSION):
+        for j in range(DIMENSION):
+            color = colors[(i+j)%2]
+            p.draw.rect(screen,color,p.Rect((j*SQ_SIZE,i*SQ_SIZE,SQ_SIZE,SQ_SIZE)))
+
+def drawPieces(screen,board):
+
+    for i in range(DIMENSION):
+        for j in range(DIMENSION):
+            if board[i][j] == '--':
+                continue
+            screen.blit(IMAGES[board[i][j]], (j*SQ_SIZE,i*SQ_SIZE,SQ_SIZE,SQ_SIZE))
+
+
+
+if __name__ == '__main__':
+    main()
